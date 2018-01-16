26cce75a4d84d777b036ac9502a4f4358eff3b50
Matthew Leon <ml@matthewleon.com>
2018-01-16 17:23:08 -0500

    benchmarks for keys and values

--- bench/Bench/Data/Map.purs
+++ bench/Bench/Data/Map.purs
@@ -17,20 +17,32 @@ benchMap = do
 
   log ""
 
+  log "keys"
+  log "------------"
+  benchKeys
+
+  log ""
+
+  log "values"
+  log "------------"
+  benchValues
+
+  log ""
+
   log "fromFoldable"
   log "------------"
   benchFromFoldable
 
   where
 
-  benchSize = do
-    let nats = L.range 0 999999
-        natPairs = (flip Tuple) unit <$> nats
-        singletonMap = M.singleton 0 unit
-        smallMap = M.fromFoldable $ L.take 100 natPairs
-        midMap = M.fromFoldable $ L.take 10000 natPairs
-        bigMap = M.fromFoldable $ natPairs
+  nats = L.range 0 999999
+  natPairs = (flip Tuple) unit <$> nats
+  singletonMap = M.singleton 0 unit
+  smallMap = M.fromFoldable $ L.take 100 natPairs
+  midMap = M.fromFoldable $ L.take 10000 natPairs
+  bigMap = M.fromFoldable $ natPairs
 
+  benchSize = do
     log "size: singleton map"
     bench \_ -> M.size singletonMap
 
@@ -42,6 +54,32 @@ benchMap = do
 
     log $ "size: big map (" <> show (M.size bigMap) <> ")"
     benchWith 10  \_ -> M.size bigMap
+
+  benchKeys = do
+    log "keys: singleton map"
+    bench \_ -> M.keys singletonMap
+
+    log $ "keys: small map (" <> show (M.size smallMap) <> ")"
+    bench \_ -> M.keys smallMap
+
+    log $ "keys: midsize map (" <> show (M.size midMap) <> ")"
+    benchWith 100 \_ -> M.keys midMap
+
+    log $ "keys: big map (" <> show (M.size bigMap) <> ")"
+    benchWith 10  \_ -> M.keys bigMap
+
+  benchValues = do
+    log "values: singleton map"
+    bench \_ -> M.values singletonMap
+
+    log $ "values: small map (" <> show (M.size smallMap) <> ")"
+    bench \_ -> M.values smallMap
+
+    log $ "values: midsize map (" <> show (M.size midMap) <> ")"
+    benchWith 100 \_ -> M.values midMap
+
+    log $ "values: big map (" <> show (M.size bigMap) <> ")"
+    benchWith 10  \_ -> M.values bigMap
 
   benchFromFoldable = do
     let natStrs = show <$> L.range 0 99999
